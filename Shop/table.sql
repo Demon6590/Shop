546f84f38e1dca65fc093b0c05de654a3d384f70
Demon6590 <dima.chernytin@gmail.com>
2026-04-24 15:51:51 +0300

    Добавление Dapper и исправление ошибок в таблице

--- Shop/table.sql
+++ Shop/table.sql
@@ -1,15 +1,15 @@
 ﻿CREATE TABLE table_price(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     price INTEGER CHECK ( price >= 0),
-    issue_date TEXT 
-                        
+    issue_date TEXT NOT NULL
+        
 );
 
 CREATE TABLE table_product
 (
     id                 INTEGER PRIMARY KEY AUTOINCREMENT,
     name               TEXT    NOT NULL CHECK ( name != '' ),
-    id_price_per_piece INTEGER,
+    id_price_per_piece INTEGER NOT NULL ,
     stock              INTEGER NOT NULL DEFAULT 0,
     FOREIGN KEY (id_price_per_piece) REFERENCES table_price(id)
 );
