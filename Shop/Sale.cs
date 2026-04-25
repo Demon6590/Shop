34d1a1809bde172f13c7fb5b431b89109a517a9f
Demon6590 <dima.chernytin@gmail.com>
2026-04-25 16:56:19 +0300

    Испровление ошибки добовлением костыля

--- Shop/Sale.cs
+++ Shop/Sale.cs
@@ -3,10 +3,7 @@
 public record Sale(){
     
     public int SaleId { get; init; }
-    // TODO Доделать добовление даты. Происходит конфликт потомучто в бд дата хронится как TEXN
-    public DateOnly SaleDate { get; init; }
-    
-    
+    public string SaleDate { get; init; }// TODO Нужно переделать тип даных на DateOnly.
     public string Buyer { get; init; }
     public string ProductName { get; init; }
 }
